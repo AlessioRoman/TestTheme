33a4aa3e4c9a155e4af7cc8afe624960ab5a43cb
AlessioRoman <alessio.romano02dev@gmail.com>
2023-04-12 12:55:25 +0200

    Implemented logic

--- TestTheme/Program.cs
+++ TestTheme/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Quante persone partecipano?");
+int numeroPartecipanti = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Persone a cui piace la Trap: ");
+float fanTrap = float.Parse(Console.ReadLine());
+Console.WriteLine("Persone a cui piace il Metal: ");
+float fanMetal = float.Parse(Console.ReadLine());
+Console.WriteLine("Persone a cui piace il Rock: ");
+float fanRock = float.Parse(Console.ReadLine());
+Console.WriteLine("Persone a cui piace la Techno: ");
+float fanTechno = float.Parse(Console.ReadLine());
+
+Console.WriteLine("---------------------STATS---------------------");
+Console.WriteLine((fanTrap/numeroPartecipanti) * 100f + "%");
+Console.WriteLine((fanMetal/numeroPartecipanti) * 100f + "%");
+Console.WriteLine((fanRock / numeroPartecipanti) * 100f + "%");
+Console.WriteLine((fanTechno / numeroPartecipanti) * 100f + "%");
+Console.WriteLine("-----------------------------------------------");
